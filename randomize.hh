87b57aca9094077fef7e7f8ab6384813b299d0f6
Shawn Yarbrough <shawn.yarbrough@gmail.com>
2023-11-24 16:46:33 -0800

    Allow specifying the seed for a Randomize instance.

--- randomize.hh
+++ randomize.hh
@@ -11,15 +11,15 @@ class Randomize {
     std::uniform_int_distribution<std::int64_t> dist;
 
 public:
-    Randomize(std::int64_t n2)
+    Randomize(std::int64_t n1, std::int64_t n2)
        : seed_value(seed())
        , gen(seed_value)
-       , dist(1, n2) {
+       , dist(n1, n2) {
         std::cout << "seed " << seed_value << std::endl;
     }
 
-    Randomize(std::int64_t n1, std::int64_t n2)
-       : seed_value(seed())
+    Randomize(std::int64_t n1, std::int64_t n2, size_t seed_value)
+       : seed_value(seed_value)
        , gen(seed_value)
        , dist(n1, n2) {
         std::cout << "seed " << seed_value << std::endl;
